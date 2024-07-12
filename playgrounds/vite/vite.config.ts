55196898e1f72b1a84fd20344326dfc82e4d96bb
Markus Katharina Brechtel <markus.katharina.brechtel@thengo.net>
2024-07-12 09:44:12 +0200

    add multiple playgrounds

--- playgrounds/vite/vite.config.ts
+++ playgrounds/vite/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import inspect from 'vite-plugin-inspect'
-import webdav from '../src/plugins/vite'
+import webdav from '../../src/plugins/vite'
 
 export default defineConfig({
   plugins: [
